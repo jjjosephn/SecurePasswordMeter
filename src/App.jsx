3db62aea7bf44dce4e71a57785ea92562514b797
ttmtu2003 <ttmtu2003@gmail.com>
2024-11-22 15:49:26 -0800

    update container width for better spacing

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { PasswordGenerator } from './components/passwordGenerator'
 function App() {
   return (
     <div className="app-container">
-      <div className="min-h-screen">
+      <div className="min-h-screen" style={{ width: '29rem' }}>
         <div className="container">
           <h1 className="title">Secure Password Meter</h1>
           <PasswordMeter />
